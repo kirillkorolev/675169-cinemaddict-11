9c2988539a8a52be2669bf089f6bcc51124857a7
Кирилл <kkirillcore@gmail.com>
2020-04-25 17:07:58 +0300

    сортировка + правки + без избранных фильмов

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {generateCategories} from "./mock/menu";
 import PageController from "./controllers/page.js";
 import StatusComponent from "./components/user-status.js";
 import MenuComponent from "./components/menu.js";
-import SortComponent from "./components/sort.js";
+//  import SortComponent from "./components/sort.js";
 import FilmsComponent from "./components/films.js";
 import NoFilmsComponent from "./components/no-films.js";
 import FilmsStatistic from "./components/films-statistic.js";
@@ -18,9 +18,9 @@ import CommentedFilmsComponent from "./components/commented-films.js";
 import {RenderPosition, render} from "./utils/render.js";
 
 const FILM_COUNT = 11;
-const EXTRAS_COUNT = 2;
+// const EXTRAS_COUNT = 2;
 
-const categories = generateCategories();
+// const categories = generateCategories();
 
 const info = generateAmmountInfo();
 
@@ -29,19 +29,17 @@ const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
 
 render(siteHeaderElement, new StatusComponent(info), RenderPosition.BEFOREEND);
-render(siteMainElement, new MenuComponent(categories), RenderPosition.BEFOREEND);
-render(siteMainElement, new SortComponent(), RenderPosition.BEFOREEND);
+// render(siteMainElement, new MenuComponent(categories), RenderPosition.BEFOREEND);
+//  render(siteMainElement, new SortComponent(), RenderPosition.BEFOREEND);
+
+const cardsMocks = generateCards(FILM_COUNT);
+const filmsSectionComponent = new FilmsComponent();
+const pageController = new PageController(filmsSectionComponent);
 
 const FilmsStatisticComponent = new FilmsStatistic(FILM_COUNT);
 
 const siteStatisticElement = siteFooterElement.querySelector(`.footer__statistics`);
 render(siteStatisticElement, FilmsStatisticComponent, RenderPosition.BEFOREEND);
-
-const cardsMocks = generateCards(FILM_COUNT);
-
-const filmsSectionComponent = new FilmsComponent();
-
-const pageController = new PageController(filmsSectionComponent);
 
 if (cardsMocks.length > 0) {
 
@@ -50,15 +48,15 @@ if (cardsMocks.length > 0) {
 
   const ratedFilms = new RatedFilmsComponent();
   const commentedFilms = new CommentedFilmsComponent();
-  const extrasMocks = cardsMocks.slice(0, EXTRAS_COUNT);
+  // const extrasMocks = cardsMocks.slice(0, EXTRAS_COUNT);
 
-  render(filmsSectionComponent.getElement(), ratedFilms, RenderPosition.BEFOREEND);
-  render(filmsSectionComponent.getElement(), commentedFilms, RenderPosition.BEFOREEND);
+  // render(filmsSectionComponent.getElement(), ratedFilms, RenderPosition.BEFOREEND);
+  // render(filmsSectionComponent.getElement(), commentedFilms, RenderPosition.BEFOREEND);
 
-  const ratedFilmsController = new PageController(ratedFilms);
-  const commentedFilmsController = new PageController(commentedFilms);
-  ratedFilmsController.render(extrasMocks);
-  commentedFilmsController.render(extrasMocks);
+  // const ratedFilmsController = new PageController(ratedFilms);
+  // const commentedFilmsController = new PageController(commentedFilms);
+  // ratedFilmsController.render(extrasMocks);
+  // commentedFilmsController.render(extrasMocks);
 } else {
   const noFilmsElement = new NoFilmsComponent();
   render(siteMainElement, noFilmsElement, RenderPosition.BEFOREEND);
